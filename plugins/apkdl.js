78059e3ea1ca77ca2434dd8ea8325015fd5e1489
supundil <83198373+supundil@users.noreply.github.com>
2021-08-23 08:43:41 +0530

    Create apkdl.js

---
+++ plugins/apkdl.js
@@ -0,0 +1,49 @@
+let axios = require("axios");
+
+let handler = async(m, { conn, text }) => {
+
+    if (!text) return conn.reply(m.chat, 'ඒම එකක් නෑ ක්ක්ක්ක්ක්', m)
+
+	axios.get(`http://hujanapi.xyz/api/apkpure?query=${text}&apikey=qrQuAVo14XfmRIr`).then ((res) => {	 	let hasil = `
+
+Nama App : ${res.data.result.data.title}
+
+Link download : ${res.data.result.data.link}
+
+Mau langsung download? Ketik .apkdl (link tersebut)`
+
+    conn.reply(m.chat, hasil, m)
+
+	})
+
+}
+
+handler.help = ['apk (kata kunci)']
+
+handler.tags = ['apk']
+
+handler.command = /^(apk)$/i
+
+handler.owner = false
+
+handler.mods = false
+
+handler.premium = false
+
+handler.group = false
+
+handler.private = false
+
+handler.admin = false
+
+handler.botAdmin = false
+
+handler.fail = null
+
+handler.exp = 0
+
+handler.limit = false
+
+module.exports = handler
+
+© 2021 GitHub, Inc.
