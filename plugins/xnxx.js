e56bdd337c03d183d101aeabcf71e5de43e71016
supu-n <83198373+supundil@users.noreply.github.com>
2021-08-31 16:33:06 +0530

    Update xnxx.js

--- plugins/xnxx.js
+++ plugins/xnxx.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn }) => {
 
-  conn.sendFile(m.chat, 'https://h4ck3rs404-api.herokuapp.com/api/xnxx-search?q=pubg+gameplay&apikey=404Api', '', 'caption', m)
+  conn.sendFile(m.chat, 'https://api.lolhuman.xyz/api/xnxx?apikey=0bc2903f193ecc977d61427a&url=?text', '', 'caption', m)
 
 }
 
